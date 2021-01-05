7fb5ee54924bb20aef7af288e94786c20850232f
mohammed K <m159.l159@gmail.com>
2021-01-05 23:48:29 +0300

    completed project

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -25,6 +25,60 @@
  * 
 */
 
+function createNav() {
+    // add <li> element
+    // add <a> element to <li>
+    const fragment = document.createDocumentFragment();
+    const sections  = document.querySelectorAll('section');
+    const ul = document.getElementById('navbar__list')
+
+    sections.forEach(section => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.setAttribute('class', 'menu__link');
+        a.setAttribute('href', '#' + section.id);
+        a.setAttribute('id',  section.id + '-nav__id' )
+        a.textContent = section.dataset.nav;
+        li.appendChild(a);
+        fragment.appendChild(li);
+    });
+    ul.appendChild(fragment);
+}
+
+function scrollHelper() {
+    document.querySelector('html').style.scrollBehavior = 'smooth';
+}
+function toggleActive() {
+    document.addEventListener('scroll', function (e) {
+
+    const sections  = document.querySelectorAll('section');
+    sections.forEach(section => {
+        if (isOnScreen(section)){
+            // toggle your-active-class class
+            document.querySelectorAll('.your-active-class')
+                .forEach(e => { e.classList.toggle('your-active-class', false)})
+            section.classList.toggle('your-active-class', true)
+
+            //toggle toggleActive class
+            document.querySelectorAll('.link_active')
+                .forEach(e => { e.classList.toggle('link_active', false)})
+            document.getElementById(section.id + '-nav__id').classList.toggle('link_active', true)
+            // document.querySelectorAll('#' + section.id + '-nav__id')
+            //     .forEach(e => {e.classList.toggle('link_active',true)})
+        }
+    })
+})
+}
+function isOnScreen(element) {
+    const elementPlace = element.getBoundingClientRect();
+    const isElementOnScreen = (elementPlace.top >= 0
+        && elementPlace.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        && elementPlace.right <= (window.innerWidth || document.documentElement.clientWidth)
+        && elementPlace.left >= 0
+    );
+    return isElementOnScreen;
+}
+
 
 
 /**
@@ -33,21 +87,24 @@
  * 
 */
 
-// build the nav
 
+function main(){
+    // build the nav
+    createNav();
 
-// Add class 'active' to section when near top of viewport
+    // Add class 'active' to section when near top of viewport
+    toggleActive();
 
-
-// Scroll to anchor ID using scrollTO event
-
+    // Scroll to anchor ID using scrollTO event
+    scrollHelper();
+}
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
+main();
 // Build menu 
 
 // Scroll to section on link click
